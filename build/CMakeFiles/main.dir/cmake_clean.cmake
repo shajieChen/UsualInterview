311ba9b4217ea3019ba59cb836582a459b93b2f4
SJ cheng <513601594@qq.com>
2020-04-05 10:54:00 +0800

    '冒泡排序练习'

--- build/CMakeFiles/main.dir/cmake_clean.cmake
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/main.dir/算法刷题/排序练习/P2676_[USACO07DEC]Bookshelf_B.cpp.o"
+  "CMakeFiles/main.dir/算法刷题/排序练习/P1116_车厢重组.cpp.o"
   "main"
   "main.pdb"
 )
--- 算法刷题/排序练习/P1116_车厢重组.cpp
+++ 算法刷题/排序练习/P1116_车厢重组.cpp
@@ -6,10 +6,28 @@
 #include <algorithm>
 using namespace std;  
 
+int BubbleSort(std::vector<int> &InputVector)
+{
+    int iCount = 0 ;  
+    for(std::size_t j  = 0 ; j < InputVector.size() - 1; j++)
+    for(std::size_t i  = 0 ; i < InputVector.size() - 1; i++)
+    {
+        if(InputVector[i] > InputVector[i+1])
+        {
+            std::swap(InputVector[i] , InputVector[i+1]);
+            iCount = iCount + 1 ; 
+        }    
+    }
+    return iCount;  
+}
+
+ 
+
 int main(int argc, char const *argv[])
 {  
     unsigned int n = 0 ;
     unsigned int iCurUsrInput = 0;
+    unsigned int iResult = 0 ; 
     std::vector<int> iUsrInputCollection ;  
     cin >> n ;
     /*Get the Usr Input Collection*/ 
@@ -18,8 +36,7 @@ int main(int argc, char const *argv[])
         cin >> iCurUsrInput ; 
         iUsrInputCollection.push_back(iCurUsrInput);   
     }
-
-    
-    
+    iResult = BubbleSort(iUsrInputCollection);
+    std::cout << iResult << std::endl ;  
     return 0 ;  
 }
